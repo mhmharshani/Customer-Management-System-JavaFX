905607760193a94d3233a3c88bfd108bdbd464bc
mhmharshani <mhmharshani@gmail.com>
2026-01-08 00:19:40 +0530

    customer table reload

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -1,5 +1,136 @@
 package controller;
 
-public class CustomerFormController {
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.CustomerTM;
 
+import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class CustomerFormController implements Initializable {
+
+    @FXML
+    private JFXComboBox cmbTitle;
+
+    @FXML
+    private TableColumn colAddress;
+
+    @FXML
+    private TableColumn colCity;
+
+    @FXML
+    private TableColumn colDob;
+
+    @FXML
+    private TableColumn colId;
+
+    @FXML
+    private TableColumn colName;
+
+    @FXML
+    private TableColumn colPostalCode;
+
+    @FXML
+    private TableColumn colProvince;
+
+    @FXML
+    private TableColumn colSalary;
+
+    @FXML
+    private DatePicker dateDob;
+
+    @FXML
+    private TableView tblCustomers;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtCity;
+
+    @FXML
+    private JFXTextField txtId;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtPostalCode;
+
+    @FXML
+    private JFXTextField txtProvince;
+
+    @FXML
+    private JFXTextField txtSalary;
+
+    @FXML
+    void btnAddCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+        loadTable();
+    }
+
+    public void loadTable(){
+
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        colCity.setCellValueFactory(new PropertyValueFactory<>("city"));
+        colDob.setCellValueFactory(new PropertyValueFactory<>("dob"));
+        colProvince.setCellValueFactory(new PropertyValueFactory<>("province"));
+        colPostalCode.setCellValueFactory(new PropertyValueFactory<>("postalCode"));
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade_db","root","199161500318");
+            System.out.println(connection);
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Customer");
+
+            ArrayList<CustomerTM> customerTMS = new ArrayList<>();
+
+            while(resultSet.next()){
+                customerTMS.add(
+                        new CustomerTM(
+                                resultSet.getString(1),
+                                resultSet.getString(2),
+                                resultSet.getString(3),
+                                resultSet.getDate(4),
+                                resultSet.getDouble(5),
+                                resultSet.getString(6),
+                                resultSet.getString(7),
+                                resultSet.getString(8),
+                                resultSet.getString(9)
+                        )
+                );
+
+            }
+            ObservableList<CustomerTM> observableList = FXCollections.observableArrayList(customerTMS);
+
+            tblCustomers.setItems(observableList);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadTable();
+    }
 }
---
+++ src/main/java/model/CustomerTM.java
@@ -0,0 +1,32 @@
+package model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+@Data
+@ToString
+@NoArgsConstructor
+//customer table model
+public class CustomerTM {
+    private String id;
+    private String name;
+    private Date dob;
+    private Double salary;
+    private String address;
+    private String city;
+    private String province;
+    private String postalCode;
+
+    public CustomerTM(String id,String title, String name, Date dob, Double salary, String address, String city, String province, String postalCode) {
+        this.id = id;
+        this.name = title+". "+name;
+        this.dob = dob;
+        this.salary = salary;
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.postalCode = postalCode;
+    }
+}
